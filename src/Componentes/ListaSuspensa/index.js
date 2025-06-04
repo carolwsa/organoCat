509209624dcf45ed3e7e2a1eddb75f2f93bd2a55
carolwsa <carolinana578@gmail.com>
2025-06-04 08:22:25 -0300

    Removendo elementos osiosos

--- src/Componentes/ListaSuspensa/index.js
+++ src/Componentes/ListaSuspensa/index.js
@@ -1,18 +1,10 @@
 import "./ListaSuspensa.css";
-import { IoIosAddCircleOutline } from "react-icons/io";
 
 const ListaSuspensa = (props) => {
   return (
     <div className="lista-suspensa">
       <div className="label-button">
         <label>{props.label}</label>
-        <button
-          type="button"
-          className="lista-suspensa-button"
-          onClick={props.aoAdicionar}
-        >
-          <IoIosAddCircleOutline size={25} />
-        </button>
       </div>
       <select
         required={props.obrigatorio}
